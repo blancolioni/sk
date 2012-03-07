5a81362041be2ee87bedc60e31dc315eb12eae14
Fraser Wilson <blancolioni@gmail.com>
2012-03-07 15:57:59 +0100

    removed unused files; added alix file

--- src/sk-io.ads
+++
@@ -1,9 +0,0 @@
-package SK.IO is
-
-   procedure Save (File_Name : String;
-                   Start     : Object);
-
-   function Load (File_Name : String) return Object;
-
-end SK.IO;
-
--- src/sk-memory_test.ads
+++
@@ -1,5 +0,0 @@
-package SK.Memory_Test is
-
-   procedure Test_Memory;
-
-end SK.Memory_Test;
